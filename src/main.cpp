a633deebe6e842b0d1bb4195efad07def4efbeab
Pavel Artemyev <a@pavel.bz>
2019-12-22 01:20:21 +0300

    working prototype

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,80 @@
 #include "main.h"
 
+#define DHTPIN 12
+#define DHTTYPE DHT22
+#define HEATER_PIN 13
+#define VIBRO_PIN 11
+#define HALFSTEP 8
+#define motorPin1  7     // IN1 on the ULN2003 driver 1
+#define motorPin2  8     // IN2 on the ULN2003 driver 1
+#define motorPin3  9     // IN3 on the ULN2003 driver 1
+#define motorPin4  10     // IN4 on the ULN2003 driver 1
+#define FIRST_FEEDING_HOUR 6
+#define FIRST_FEEDING_MINUTE 1
+#define SECOND_FEEDING_HOUR 21
+#define SECOND_FEEDING_MINUTE 55
+DHT_Unified dht(DHTPIN, DHTTYPE);
+// Initialize with pin sequence IN1-IN3-IN2-IN4 for using the 28BYJ-48 AccelStepper with ULN2003 driver (4096 steps per circle)
+AccelStepper feeder_stepper(HALFSTEP, motorPin1, motorPin3, motorPin2, motorPin4);
+
+
+static const char * TIME_HEADER = "T";   // Header tag for serial time sync message
+static unsigned delayMS;
+
 void setup() {
     Serial.begin(115200);
+    dht.begin();
+    sensor_t sensor;
+    dht.temperature().getSensor(&sensor);
+    delayMS = sensor.min_delay / 1000;
     while (!Serial); // wait until Arduino Serial Monitor opens
     setSyncProvider(RTC.get);   // the function to get the time from the RTC
     if(timeStatus()!= timeSet)
         Serial.println("Unable to sync with the RTC");
     else
         Serial.println("RTC has set the system time!");
+    pinMode(HEATER_PIN, OUTPUT);
+    digitalWrite(HEATER_PIN, LOW);
+    pinMode(VIBRO_PIN, OUTPUT);
+    digitalWrite(VIBRO_PIN, LOW);
+    feeder_stepper.setMaxSpeed(600.0);
+    feeder_stepper.setAcceleration(100.0);
+    feeder_stepper.setSpeed(200);
 }
 
 void loop()
 {
+      // Delay between measurements.
+      delay(delayMS);
+      // Get temperature event and print its value.
+      sensors_event_t event;
+      dht.temperature().getEvent(&event);
+      if (isnan(event.temperature)) {
+        Serial.println("Error reading temperature!");
+      }
+      else {
+        Serial.print("Temperature: ");
+        Serial.print(event.temperature);
+        Serial.println("°C");
+        if(event.temperature <= 25) {
+            digitalWrite(HEATER_PIN, HIGH);
+            Serial.println("Heater turned on.");
+        }
+        else if (event.temperature >= 26) {
+            digitalWrite(HEATER_PIN, LOW);
+            Serial.println("Heater turned off.");
+        }
+      }
+      // Get humidity event and print its value.
+      dht.humidity().getEvent(&event);
+      if (isnan(event.relative_humidity)) {
+        Serial.println("Error reading humidity!");
+      }
+      else {
+        Serial.print("Humidity: ");
+        Serial.print(event.relative_humidity);
+        Serial.println("%");
+      }
     if (Serial.available()) {
         time_t t = processSyncMessage();
         if (t != 0) {
@@ -20,10 +83,14 @@ void loop()
         }
     }
     if (timeStatus() == timeSet) {
+        if (hour() == FIRST_FEEDING_HOUR and minute() == FIRST_FEEDING_MINUTE){
+            feedFish(3);
+        } else if (hour() == SECOND_FEEDING_HOUR and minute() == SECOND_FEEDING_MINUTE){
+            feedFish(3);
+        }
         digitalClockDisplay();
     } else {
-        Serial.println("The time has not been set.  Please run the Time");
-        Serial.println("TimeRTCSet example, or DS1307RTC SetTime example.");
+        Serial.println("The time has not been set.  Please run the Time by sending T${timestamp}");
         Serial.println();
         delay(4000);
     }
@@ -51,11 +118,11 @@ void digitalClockDisplay(){
     printDigits(minute());
     printDigits(second());
     Serial.print(" ");
-    Serial.print(day());
-    Serial.print(" ");
-    Serial.print(month());
-    Serial.print(" ");
     Serial.print(year());
+    Serial.print("-");
+    Serial.print(month());
+    Serial.print("-");
+    Serial.print(day());
     Serial.println();
 }
 
@@ -65,4 +132,34 @@ void printDigits(int digits){
     if(digits < 10)
         Serial.print('0');
     Serial.print(digits);
+}
+
+void vibrate(unsigned long duration_millis){
+    unsigned long start_time = millis();
+    while (millis() - start_time < duration_millis){
+        digitalWrite(VIBRO_PIN, HIGH);
+        delay(100);
+        digitalWrite(VIBRO_PIN, LOW);
+        delay(50);
+    }
+}
+
+void feedFish(int servings){
+    Serial.println("Feeding time!");
+    for (int i = 0; i < servings; i++){
+        rotateFeederToStockPile();
+        vibrate(1000);
+        rotateFeederToOpening();
+        vibrate(1000);
+        rotateFeederToStockPile();
+    }
+    delay(40000);
+}
+
+void rotateFeederToStockPile(){
+    feeder_stepper.runToNewPosition(0);
+}
+
+void rotateFeederToOpening(){
+    feeder_stepper.runToNewPosition(2250);
 }
--- src/main.h
+++ src/main.h
@@ -5,11 +5,19 @@
 #include <TimeLib.h>
 #include <Wire.h>
 #include <DS3232RTC.h>  // a basic DS3232RTC library that returns time as a time_t
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <AccelStepper.h>
 
 void printDigits(int digits);
 void digitalClockDisplay();
+void rotateFeederToStockPile();
+void rotateFeederToOpening();
+void vibrate(unsigned long duration_millis);
+void feedFish(int servings);
 unsigned long processSyncMessage();
-
-static const char * TIME_HEADER = "T";   // Header tag for serial time sync message
+extern DHT_Unified dht;
+extern AccelStepper feeder_stepper;
 
 #endif // MAIN_H
